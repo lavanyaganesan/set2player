12158f60ce9aede67145bc5298ede90416f224d3
lavanyaganesan <lavanyagp2257@gmail.com>
2017-07-04 09:45:04 +0530

    Create StringInt.java

---
+++ StringInt.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class StringInt
+{
+public static void main(String args[])
+{
+String a="145";
+Integer result=Integer.valueOf(a);
+System.out.println("String is: "+a);
+System.out.println("String to Integer conversion: "+result);
+}
+}
