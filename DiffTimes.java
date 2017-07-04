01a0e11735cb0354d2454fba985839a0f2bc600b
lavanyaganesan <lavanyagp2257@gmail.com>
2017-07-04 21:47:56 +0530

    Update DiffTimes.java

--- DiffTimes.java
+++ DiffTimes.java
@@ -5,18 +5,18 @@ public class DiffTimes
 public static void main(String args[])
 {
 Scanner sc=new Scanner(System.in);
-int h1,m1,h2,m2,tmin,tmin2,min;
+int h1,m1,h2,m2,tmin1,tmin2,min;
 System.out.println("Enter the first time (hours and seconds));
 h1=sc.nextInt();
 m1=sc.nextInt();
-tmin=h1*60;
-tmin=tmin+tmin;
+tmin1=h1*60;
+tmin1=tmin1+tmin1;
 System.out.println("Enter the second time (hours and seconds));
 h2=sc.nextInt();
 m2=sc.nextInt();
 tmin1=h2*60;
-tmin1=tmin1+tmin1;
-min=tmin1-tmin;
+tmin2=tmin2+tmin2;
+min=tmin2-tmin1;
 System.out.println("The minute difference between 2 time is : "+min);
 }
 }
