6f433b675f08742e39d07794b4348fcff444bfab
lavanyaganesan <lavanyagp2257@gmail.com>
2017-07-07 10:48:24 +0530

    Update BinaryDes.java

--- BinaryDes.java
+++ BinaryDes.java
@@ -1,35 +1,31 @@
-import java.util.*;
-import java.io.*;
-public class BinaryDes
-{
-public static void main(String args[]) throws IOException
-{
-Scanner sc=new Scanner(System.in);
-System.out.println("Enter the number of elements u want?");
-int n=sc.nextInt();
-int a[]=new int[n];
-System.out.println("Enter the elements");
-for(int i=0;i<n;i++)
-{
-a[i]=sc.nextInt();
-}
-for(int i=0;i<n;i++)
-{
-for(int i+1;j<n;j++)
-{
-if(a[i]<a[j])
-{
-count=a[i];
-a[i]=a[j];
-a[j]=count;
-}
-}
-}
-System.out.println("Descending order:");
-for(int i=0;i<n-1;i++)
-{
-System.out.print(a[i]+",");
-}
-System.out.println(a[n-1]);
-}
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class BinaryDes {
+  public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		Integer n,i;
+		ArrayList<Integer> list=new ArrayList<Integer>();
+		System.out.println("Enter the number of array");
+		n=sc.nextInt();
+		System.out.println("Enter the array of elements");
+		int a[]=new int[n];
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+			list.add(a[i]);
+		}
+		Collections.sort(list);
+        Collections.reverse(list);
+		System.out.println("Desending order number is");
+		list.toString();
+		for(Integer x:list)
+		{
+			System.out.println(x);
+		}
+		
+	}
+
 }
